8df95b91552b12e5ad99278809992468f8deae5d
cgpurbaugh <cpurbaugh@sea-fire.com>
2017-04-17 13:21:56 -0400

    Added DocTypes to app

---
+++ metalcraft/metalcraft/doctype/erp_instruction_manual/erp_instruction_manual.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Metalcraft and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ERP Instruction Manual', {
+	refresh: function(frm) {
+
+	}
+});
