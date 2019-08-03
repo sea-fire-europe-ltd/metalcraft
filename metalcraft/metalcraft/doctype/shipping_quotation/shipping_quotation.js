54c6141be4e3f1bce96cc92da1065ec2a979bd3b
Virtual <virtual@ip-172-31-31-186.ec2.internal>
2019-08-03 16:07:56 +0000

    changes from update 8-3-19

---
+++ metalcraft/metalcraft/doctype/shipping_quotation/shipping_quotation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Metalcraft, Inc. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shipping Quotation', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ metalcraft/metalcraft/doctype/shipping_quotation/test_shipping_quotation.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Metalcraft, Inc. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestShippingQuotation(unittest.TestCase):
+	pass
